60a22aeaacf8d5992a2b361dd2acaae29f4bd6af
Nikita-numberOne <jasonzhora@mail.ru>
2024-09-09 23:28:52 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+immutable_var = 1, "apple", 3, 4, "peach",
+print(immutable_var)
+# immutable_var [0] = 2
+# print(immutable_var)mutable_list = ["apple", "peach", "banana", "coconut"]
+# Значения элементов кортежа нельзя изменить потому что кортеж относится к неизменяемым типам данных
+mutable_list = ["apple", "peach", "banana", "coconut"]
+print(mutable_list)
+mutable_list [0] = 2
+mutable_list [3] = "apple"
+print(mutable_list)
+
+
+
